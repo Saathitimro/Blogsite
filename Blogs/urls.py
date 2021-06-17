e1a8c4275d3d70c4bb4df11c4773f1e513d73834
Basanta-afk <67224509+Basanta-afk@users.noreply.github.com>
2021-06-17 17:59:06 +0545

    bijay ko portion integreted

--- Blogs/urls.py
+++ Blogs/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.home),
     path('home', views.home),
     path('blog', views.blog),
+    path('login', views.login),
+    path('profile', views.profile),
+    path('schedule', views.schedule),
+    path('pending', views.pending),
+    path('detail', views.detail),
     path('<pk>/details/', views.details),
 ]
--- Blogs/views.py
+++ Blogs/views.py
@@ -6,6 +6,9 @@ def home(request):
     context = {'blogs' : BlogModel.objects.all()}
     return render(request, "Pages/Index.html" , context)
 
+def login(request):
+    return render(request, "Pages/counselor/login.html")
+
 def blog(request):
     context = {'blogs' : BlogModel.objects.all()}
     return render(request, "Pages/Blogs.html", context)
@@ -13,3 +16,15 @@ def blog(request):
 def details(request,pk):
     context = {'blogs' : BlogModel.objects.get(id=pk)}
     return render(request,"Pages/blogpost.html", context)
+
+def profile(request):
+    return render(request, "Pages/counselor/profile.html")
+
+def detail(request):
+    return render(request, "Pages/counselor/detail.html")
+
+def pending(request):
+    return render(request, "Pages/counselor/pending.html")
+
+def schedule(request):
+    return render(request, "Pages/counselor/schedule.html")
